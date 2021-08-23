33014d23851f319b053edbd11eeabf0382be1549
alexandrebastosborges <54225467+alexandrebastosborges@users.noreply.github.com>
2021-08-23 01:10:50 -0300

    Add files via upload

---
+++ ios/Pods/Target Support Files/esys_flutter_share/esys_flutter_share-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_esys_flutter_share : NSObject
+@end
+@implementation PodsDummy_esys_flutter_share
+@end
---
+++ ios/Pods/Target Support Files/esys_flutter_share/esys_flutter_share-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "EsysFlutterSharePlugin.h"
+
+FOUNDATION_EXPORT double esys_flutter_shareVersionNumber;
+FOUNDATION_EXPORT const unsigned char esys_flutter_shareVersionString[];
+
